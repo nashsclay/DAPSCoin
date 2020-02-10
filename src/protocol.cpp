3d5c7c5489d4d57b61e12722a0d24a86445b8eaa
lyricidal <lyricidal@gmail.com>
2020-02-09 19:15:53 -0500

    [P2P] Guard CInv::ToString against unknown types

--- src/protocol.cpp
+++ src/protocol.cpp
@@ -140,8 +140,10 @@ bool CInv::IsMasterNodeType() const{
 
 const char* CInv::GetCommand() const
 {
-    if (!IsKnownType())
+    if (!IsKnownType()) {
         LogPrint("net", "CInv::GetCommand() : type=%d unknown type", type);
+        return "UNKNOWN";
+    }
 
     return ppszTypeName[type];
 }
