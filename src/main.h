8a8a44e83dbfb5e4b65394964de17d908932ba9c
lyricidal <lyricidal@gmail.com>
2020-03-03 18:06:45 -0500

    doc: Add comment to cs_main and mempool::cs

--- src/main.h
+++ src/main.h
@@ -132,7 +132,7 @@ struct BlockHasher {
 };
 
 extern CScript COINBASE_FLAGS;
-extern CCriticalSection cs_main;
+extern RecursiveMutex cs_main;
 extern CTxMemPool mempool;
 typedef boost::unordered_map<uint256, CBlockIndex*, BlockHasher> BlockMap;
 extern BlockMap mapBlockIndex;
