175c6aca0da9122851847f227ba7ed05f78abed9
lyricidal <lyricidal@gmail.com>
2020-03-03 18:40:46 -0500

    Using WAIT_LOCK macro instead of WaitableLock.

--- src/miner.cpp
+++ src/miner.cpp
@@ -612,7 +612,7 @@ bool ProcessBlockFound(CBlock* pblock, CWallet& wallet, CReserveKey& reservekey)
 
     // Found a solution
     {
-        WaitableLock lock(g_best_block_mutex);
+        WAIT_LOCK(g_best_block_mutex, lock);
         if (pblock->hashPrevBlock != g_best_block)
             return error("DAPScoinMiner : generated block is stale");
     }
