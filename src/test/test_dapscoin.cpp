ac3f282d95b1ea1b427b8112d7046b349426a37f
lyricidal <lyricidal@gmail.com>
2020-10-24 19:40:16 -0400

    Add attribute [[noreturn]] (C++11) to functions that will not return
    Rationale:
    * Reduce the number of false positives from static analyzers
    * Potentially enable additional compiler optimizations

--- src/test/test_dapscoin.cpp
+++ src/test/test_dapscoin.cpp
@@ -130,14 +130,14 @@ struct TestingSetup {
 
 BOOST_GLOBAL_FIXTURE(TestingSetup);
 
-// void Shutdown(void* parg)
+// [[noreturn]] void Shutdown(void* parg)
 // {
-//   exit(0);
+//   std::exit(0);
 // }
 
-// void StartShutdown()
+// [[noreturn]] void StartShutdown()
 // {
-//   exit(0);
+//   std::exit(0);
 // }
 
 // bool ShutdownRequested()
