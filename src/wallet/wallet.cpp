5bc748dda3a60581cd59e9ac364e2c6c8785d072
lopeed <lopeed@hotmail.com>
2021-01-14 08:36:37 +0100

    comment staking mode

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -4014,13 +4014,13 @@ bool CWallet::CreateCoinStake(const CKeyStore& keystore, unsigned int nBits, int
             txNew.vout[1].nValue = nCredit;
             txNew.vout[2].nValue = nReward;
             LogPrintf("DEBUGGING: %s, nCredit: %d, nReward: %d\n", __func__, nCredit, nReward);
-              if (stakingMode == STAKING_WITH_CONSOLIDATION || STAKING_WITH_CONSOLIDATION_WITH_STAKING_NEWW_FUNDS) {
+              /*if (stakingMode == STAKING_WITH_CONSOLIDATION || STAKING_WITH_CONSOLIDATION_WITH_STAKING_NEWW_FUNDS) {
                 //the first output contains all funds (input + rewards + fee)
                 if (nCredit + nReward > (MINIMUM_STAKE_AMOUNT + 100000*COIN)*2) {
                     txNew.vout[1].nValue = (nCredit + nReward)/2;
                     txNew.vout[2].nValue = (nCredit + nReward) - txNew.vout[1].nValue;
                 }
-            }
+            }*/
 
             // Limit size
             unsigned int nBytes = ::GetSerializeSize(txNew, SER_NETWORK, PROTOCOL_VERSION);
