cf8233d180168351ffbbab0199086cbb34813ab8
lopeed <lopeed@hotmail.com>
2021-02-03 16:24:27 +0100

    optimize setgenerate staking status

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -139,7 +139,6 @@ UniValue setgenerate(const UniValue& params, bool fHelp)
         throw JSONRPCError(RPC_METHOD_NOT_FOUND, "Method not found (disabled)");
 
     bool fGenerate = true;
-	pwalletMain->WriteStakingStatus(true);
     if (params.size() > 0)
         fGenerate = params[0].get_bool();
 
@@ -149,9 +148,8 @@ UniValue setgenerate(const UniValue& params, bool fHelp)
         if (nGenProcLimit == 0)
             fGenerate = false;
     }
-	if (fGenerate =  false)
-		pwalletMain->WriteStakingStatus(false);
-
+	pwalletMain->WriteStakingStatus(fGenerate);
+	
     // -regtest mode: don't return until nGenProcLimit blocks are generated
     if (fGenerate && Params().MineBlocksOnDemand()) {
         int nHeightStart = 0;
