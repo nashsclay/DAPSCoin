273db219d645ea3e30755c60e1f5f93fe3c4dbae
lyricidal <lyricidal@gmail.com>
2021-08-17 11:13:59 -0400

    Add missed version to ban

--- src/net.cpp
+++ src/net.cpp
@@ -127,7 +127,7 @@ unsigned short GetListenPort() {
 
 bool IsUnsupportedVersion(std::string strSubVer, int nHeight) {
     if (nHeight >= Params().HardFork()) {
-        return (strSubVer == "/PRCY:1.0.0.2/" || strSubVer == "/PRCY:1.0.0.3/" || strSubVer == "/PRCY:1.0.0.4/" || strSubVer == "/PRCY:1.0.0.5/" || strSubVer == "/PRCY:1.0.0.7/");
+        return (strSubVer == "/PRCY:1.0.0.2/" || strSubVer == "/PRCY:1.0.0.3/" || strSubVer == "/PRCY:1.0.0.4/" || strSubVer == "/PRCY:1.0.0.5/" || strSubVer == "/PRCY:1.0.0.6/" || strSubVer == "/PRCY:1.0.0.7/");
     }
     return (strSubVer == "/PRCY:1.0.0.2/" || strSubVer == "/PRCY:1.0.0.3/" || strSubVer == "/PRCY:1.0.0.4/" || strSubVer == "/PRCY:1.0.0.5/" || strSubVer == "/PRCY:1.0.0.6/");
 }
