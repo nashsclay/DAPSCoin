81ce9856ca515a9f6dca0bcb308a27879ac57182
lyricidal <lyricidal@gmail.com>
2021-09-28 17:29:15 -0400

    allow libevent logging to be updated during runtime
    backports https://github.com/bitcoin/bitcoin/commit/7fd50c3b70397c0dc39093a7a4ddbf9b36a615cf

--- src/util.cpp
+++ src/util.cpp
@@ -129,7 +129,7 @@ bool fLogTimestamps = false;
 bool fLogIPs = false;
 volatile bool fReopenDebugLog = false;
 
-/** Log categories bitfield. libevent needs special handling if their flags are changed at runtime. */
+/** Log categories bitfield. */
 std::atomic<uint32_t> logCategories(0);
 
 /** Init OpenSSL library multithreading support */
