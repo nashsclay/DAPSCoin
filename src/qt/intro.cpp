e4a84c78ec94694b6d76fc269ab71b8790f0753a
lyricidal <lyricidal@gmail.com>
2021-11-10 19:00:11 -0500

    [GUI] Show disk space requirement per-network
    This introduces per-network required disk space into the intro UI in the least intrusive way for now. Mainnet and Testnet have their own static const variables, whereas Regtest is always set to "0".

--- src/qt/intro.cpp
+++ src/qt/intro.cpp
@@ -17,9 +17,13 @@
 #include <QMessageBox>
 #include <QSettings>
 
-/* Minimum free space (in bytes) needed for data directory */
 static const uint64_t GB_BYTES = 1000000000LL;
-static const uint64_t BLOCK_CHAIN_SIZE = 1LL * GB_BYTES;
+/* Minimum free space (in GB) needed for mainnet data directory */
+static const uint64_t BLOCK_CHAIN_SIZE = 2;
+/* Minimum free space (in GB) needed for testnet data directory */
+static const uint64_t TESTNET_BLOCK_CHAIN_SIZE = 1;
+/* Total required space (in GB) depending on network */
+static uint64_t requiredSpace;
 
 /* Check free space asynchronously to prevent hanging the UI thread.
 
@@ -108,7 +112,7 @@ Intro::Intro(QWidget* parent) : QDialog(parent, Qt::WindowSystemMenuHint | Qt::W
                                 signalled(false)
 {
     ui->setupUi(this);
-    ui->sizeWarningLabel->setText(ui->sizeWarningLabel->text().arg(BLOCK_CHAIN_SIZE / GB_BYTES));
+    ui->sizeWarningLabel->setText(ui->sizeWarningLabel->text().arg(requiredSpace));
     startThread();
 }
 
@@ -158,6 +162,13 @@ bool Intro::pickDataDirectory()
 
     if (!fs::exists(GUIUtil::qstringToBoostPath(dataDir)) || GetBoolArg("-choosedatadir", false)) {
         /* If current default data directory does not exist, let the user choose one */
+        if (GetBoolArg("-testnet", false)) {
+            requiredSpace = TESTNET_BLOCK_CHAIN_SIZE;
+        } else if (GetBoolArg("-regtest", false)) {
+            requiredSpace = 0;
+        } else {
+            requiredSpace = BLOCK_CHAIN_SIZE;
+        }
         Intro intro;
         intro.setDataDirectory(dataDir);
         intro.setWindowIcon(QIcon(":icons/bitcoin"));
@@ -206,8 +217,8 @@ void Intro::setStatus(int status, const QString& message, quint64 bytesAvailable
         ui->freeSpace->setText("");
     } else {
         QString freeString = tr("%1 GB of free space available").arg(bytesAvailable / GB_BYTES);
-        if (bytesAvailable < BLOCK_CHAIN_SIZE) {
-            freeString += " " + tr("(of %1 GB needed)").arg(BLOCK_CHAIN_SIZE / GB_BYTES);
+        if (bytesAvailable < requiredSpace * GB_BYTES) {
+            freeString += " " + tr("(of %1 GB needed)").arg(requiredSpace);
             ui->freeSpace->setStyleSheet("QLabel { color: #800000 }");
         } else {
             ui->freeSpace->setStyleSheet("");
