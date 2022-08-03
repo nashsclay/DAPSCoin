4b9fc63e615dae881bf7e236cddfd08fed01d57f
lyricidal <lyricidal@gmail.com>
2022-08-03 12:43:24 -0400

    Acquire cs_vNodes before changing refrence counts

--- src/net.cpp
+++ src/net.cpp
@@ -782,8 +782,15 @@ static std::list<CNode*> vNodesDisconnected;
 
 class CNodeRef {
 public:
-    CNodeRef(CNode *pnode) : _pnode(pnode)  {_pnode->AddRef();}
-    ~CNodeRef() {_pnode->Release();}
+    CNodeRef(CNode *pnode) : _pnode(pnode) {
+        LOCK(cs_vNodes);
+        _pnode->AddRef();
+    }
+
+    ~CNodeRef() {
+        LOCK(cs_vNodes);
+        _pnode->Release();
+    }
 
     CNode& operator *() const {return *_pnode;};
     CNode* operator ->() const {return _pnode;};
@@ -792,6 +799,7 @@ public:
     CNodeRef& operator =(const CNodeRef& other)
     {
         if (this != &other) {
+            LOCK(cs_vNodes);
             _pnode->Release();
             _pnode = other._pnode;
             _pnode->AddRef();
@@ -802,6 +810,7 @@ public:
     CNodeRef(const CNodeRef& other):
         _pnode(other._pnode)
     {
+        LOCK(cs_vNodes);
         _pnode->AddRef();
     }
 private:
