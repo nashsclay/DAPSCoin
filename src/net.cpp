b34b846a0a2143d3feefa5a463ec21c1fd36e8ed
lyricidal <lyricidal@gmail.com>
2022-08-18 23:11:02 -0400

    Remove bogus assert on number of oubound connections.
    This value can be significantly higher if the users uses addnode

--- src/net.cpp
+++ src/net.cpp
@@ -1520,7 +1520,6 @@ void ThreadOpenConnections() {
                 }
             }
         }
-        assert(nOutbound <= (MAX_OUTBOUND_CONNECTIONS + MAX_FEELER_CONNECTIONS));
 
         // Feeler Connections
         //
