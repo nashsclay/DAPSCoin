6512ebf897797499463da8597b0fb4620e93c840
lyricidal <lyricidal@gmail.com>
2022-09-01 08:22:50 -0700

    Replaced tabs with 4 white spaces, also fixed the script tabulation

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -35,23 +35,23 @@ Run this script from the directory containing the prcycoin, gitian-builder, giti
 
 Arguments:
 signer          GPG signer to sign each build assert file
-version		Version number, commit, or branch to build. If building a commit or branch, the -c option must be specified
+version        Version number, commit, or branch to build. If building a commit or branch, the -c option must be specified
 
 Options:
--c|--commit	Indicate that the version argument is for a commit or branch
--u|--url	Specify the URL of the repository. Default is https://github.com/prcycoin/prcycoin
--v|--verify 	Verify the gitian build
--b|--build	Do a gitian build
--s|--sign	Make signed binaries for Windows and Mac OSX
--B|--buildsign	Build both signed and unsigned binaries
--o|--os		Specify which Operating Systems the build is for. Default is lwx. l for linux, w for windows, x for osx, a for aarch64
--j		Number of processes to use. Default 2
--m		Memory to allocate in MiB. Default 2000
+-c|--commit    Indicate that the version argument is for a commit or branch
+-u|--url    Specify the URL of the repository. Default is https://github.com/prcycoin/prcycoin
+-v|--verify     Verify the gitian build
+-b|--build    Do a gitian build
+-s|--sign    Make signed binaries for Windows and Mac OSX
+-B|--buildsign    Build both signed and unsigned binaries
+-o|--os        Specify which Operating Systems the build is for. Default is lwx. l for linux, w for windows, x for osx, a for aarch64
+-j        Number of processes to use. Default 2
+-m        Memory to allocate in MiB. Default 2000
 --kvm           Use KVM instead of LXC
 --setup         Setup the gitian building environment. Uses KVM. If you want to use lxc, use the --lxc option. Only works on Debian-based systems (Ubuntu, Debian)
 --detach-sign   Create the assert file for detached signing. Will not commit anything.
 --no-commit     Do not commit anything to git
--h|--help	Print this help message
+-h|--help    Print this help message
 EOF
 
 # Get options and arguments
@@ -59,104 +59,104 @@ while :; do
     case $1 in
         # Verify
         -v|--verify)
-	    verify=true
+        verify=true
             ;;
         # Build
         -b|--build)
-	    build=true
+        build=true
             ;;
         # Sign binaries
         -s|--sign)
-	    sign=true
+        sign=true
             ;;
         # Build then Sign
         -B|--buildsign)
-	    sign=true
-	    build=true
+        sign=true
+        build=true
             ;;
         # PGP Signer
         -S|--signer)
-	    if [ -n "$2" ]
-	    then
-		SIGNER=$2
-		shift
-	    else
-		echo 'Error: "--signer" requires a non-empty argument.'
-		exit 1
-	    fi
+        if [ -n "$2" ]
+        then
+        SIGNER=$2
+        shift
+        else
+        echo 'Error: "--signer" requires a non-empty argument.'
+        exit 1
+        fi
            ;;
         # Operating Systems
         -o|--os)
-	    if [ -n "$2" ]
-	    then
-		linux=false
-		windows=false
-		osx=false
-		aarch64=false
-		if [[ "$2" = *"l"* ]]
-		then
-		    linux=true
-		fi
-		if [[ "$2" = *"w"* ]]
-		then
-		    windows=true
-		fi
-		if [[ "$2" = *"x"* ]]
-		then
-		    osx=true
-		fi
-		if [[ "$2" = *"a"* ]]
-		then
-		    aarch64=true
-		fi
-		shift
-	    else
-		echo 'Error: "--os" requires an argument containing an l (for linux), w (for windows), x (for Mac OSX), or a (for aarch64)\n'
-		exit 1
-	    fi
-	    ;;
-	# Help message
-	-h|--help)
-	    echo "$usage"
-	    exit 0
-	    ;;
-	# Commit or branch
-	-c|--commit)
-	    commit=true
-	    ;;
-	# Number of Processes
-	-j)
-	    if [ -n "$2" ]
-	    then
-		proc=$2
-		shift
-	    else
-		echo 'Error: "-j" requires an argument'
-		exit 1
-	    fi
-	    ;;
-	# Memory to allocate
-	-m)
-	    if [ -n "$2" ]
-	    then
-		mem=$2
-		shift
-	    else
-		echo 'Error: "-m" requires an argument'
-		exit 1
-	    fi
-	    ;;
-	# URL
-	-u)
-	    if [ -n "$2" ]
-	    then
-		url=$2
-		shift
-	    else
-		echo 'Error: "-u" requires an argument'
-		exit 1
-	    fi
-	    ;;
+        if [ -n "$2" ]
+        then
+        linux=false
+        windows=false
+        osx=false
+        aarch64=false
+        if [[ "$2" = *"l"* ]]
+        then
+            linux=true
+        fi
+        if [[ "$2" = *"w"* ]]
+        then
+            windows=true
+        fi
+        if [[ "$2" = *"x"* ]]
+        then
+            osx=true
+        fi
+        if [[ "$2" = *"a"* ]]
+        then
+            aarch64=true
+        fi
+        shift
+        else
+        echo 'Error: "--os" requires an argument containing an l (for linux), w (for windows), x (for Mac OSX), or a (for aarch64)\n'
+        exit 1
+        fi
+        ;;
+    # Help message
+    -h|--help)
+        echo "$usage"
+        exit 0
+        ;;
+    # Commit or branch
+    -c|--commit)
+        commit=true
+        ;;
+    # Number of Processes
+    -j)
+        if [ -n "$2" ]
+        then
+        proc=$2
+        shift
+        else
+        echo 'Error: "-j" requires an argument'
+        exit 1
+        fi
+        ;;
+    # Memory to allocate
+    -m)
+        if [ -n "$2" ]
+        then
+        mem=$2
+        shift
+        else
+        echo 'Error: "-m" requires an argument'
+        exit 1
+        fi
+        ;;
+    # URL
+    -u)
+        if [ -n "$2" ]
+        then
+        url=$2
+        shift
+        else
+        echo 'Error: "-u" requires an argument'
+        exit 1
+        fi
+        ;;
         # kvm
         --kvm)
             lxc=false
@@ -174,7 +174,7 @@ while :; do
         --setup)
             setup=true
             ;;
-	*)               # Default case: If no more options then break out of the loop.
+    *)               # Default case: If no more options then break out of the loop.
              break
     esac
     shift
@@ -189,7 +189,7 @@ then
 fi
 
 # Check for OSX SDK
-if [[ ! -e "gitian-builder/inputs/Xcode-11.3.1-11C505-extracted-SDK-with-libcxx-headers.tar.gz" && $osx == true ]]
+if [[ ! -e "gitian-builder/inputs/MacOSX10.11.sdk.tar.gz" && $osx == true ]]
 then
     echo "Cannot build for OSX, SDK does not exist. Will build for other OSes"
     osx=false
@@ -229,7 +229,7 @@ fi
 # Add a "v" if no -c
 if [[ $commit = false ]]
 then
-	COMMIT="v${VERSION}"
+    COMMIT="v${VERSION}"
 fi
 echo ${COMMIT}
 
@@ -237,8 +237,8 @@ echo ${COMMIT}
 if [[ $setup = true ]]
 then
     sudo apt-get install ruby apache2 git apt-cacher-ng python-vm-builder qemu-kvm qemu-utils
-    git clone https://github.com/prcycoin-project/gitian.sigs.git
-    git clone https://github.com/prcycoin-project/prcycoin-detached-sigs.git
+    git clone https://github.com/prcycoin/gitian.sigs.git
+    git clone https://github.com/prcycoin/prcycoin-detached-sigs.git
     git clone https://github.com/devrandom/gitian-builder.git
     pushd ./gitian-builder
     if [[ -n "$USE_LXC" ]]
@@ -260,154 +260,154 @@ popd
 # Build
 if [[ $build = true ]]
 then
-	# Make output folder
-	mkdir -p ./prcycoin-binaries/${VERSION}
+    # Make output folder
+    mkdir -p ./prcycoin-binaries/${VERSION}
 
-	# Build Dependencies
-	echo ""
-	echo "Building Dependencies"
-	echo ""
-	pushd ./gitian-builder
-	mkdir -p inputs
-	wget -N -P inputs $osslPatchUrl
-	wget -N -P inputs $osslTarUrl
-	make -C ../prcycoin/depends download SOURCES_PATH=`pwd`/cache/common
+    # Build Dependencies
+    echo ""
+    echo "Building Dependencies"
+    echo ""
+    pushd ./gitian-builder
+    mkdir -p inputs
+    wget -N -P inputs $osslPatchUrl
+    wget -N -P inputs $osslTarUrl
+    make -C ../prcycoin/depends download SOURCES_PATH=`pwd`/cache/common
 
-	# Linux
-	if [[ $linux = true ]]
-	then
-            echo ""
-	    echo "Compiling ${VERSION} Linux"
-	    echo ""
-	    ./bin/gbuild -j ${proc} -m ${mem} --commit prcycoin=${COMMIT} --url prcycoin=${url} ../prcycoin/contrib/gitian-descriptors/gitian-linux.yml
-	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-linux --destination ../gitian.sigs/ ../prcycoin/contrib/gitian-descriptors/gitian-linux.yml
-	    mv build/out/prcycoin-*.tar.gz build/out/src/prcycoin-*.tar.gz ../prcycoin-binaries/${VERSION}
-	fi
-	# Windows
-	if [[ $windows = true ]]
-	then
-	    echo ""
-	    echo "Compiling ${VERSION} Windows"
-	    echo ""
-	    ./bin/gbuild -j ${proc} -m ${mem} --commit prcycoin=${COMMIT} --url prcycoin=${url} ../prcycoin/contrib/gitian-descriptors/gitian-win.yml
-	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-win-unsigned --destination ../gitian.sigs/ ../prcycoin/contrib/gitian-descriptors/gitian-win.yml
-	    mv build/out/prcycoin-*-win-unsigned.tar.gz inputs/prcycoin-win-unsigned.tar.gz
-	    mv build/out/prcycoin-*.zip build/out/prcycoin-*.exe ../prcycoin-binaries/${VERSION}
-	fi
-	# Mac OSX
-	if [[ $osx = true ]]
-	then
-	    echo ""
-	    echo "Compiling ${VERSION} Mac OSX"
-	    echo ""
-	    ./bin/gbuild -j ${proc} -m ${mem} --commit prcycoin=${COMMIT} --url prcycoin=${url} ../prcycoin/contrib/gitian-descriptors/gitian-osx.yml
-	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-osx-unsigned --destination ../gitian.sigs/ ../prcycoin/contrib/gitian-descriptors/gitian-osx.yml
-	    mv build/out/prcycoin-*-osx-unsigned.tar.gz inputs/prcycoin-osx-unsigned.tar.gz
-	    mv build/out/prcycoin-*.tar.gz build/out/prcycoin-*.dmg ../prcycoin-binaries/${VERSION}
-	fi
-	# AArch64
-	if [[ $aarch64 = true ]]
-	then
-	    echo ""
-	    echo "Compiling ${VERSION} AArch64"
-	    echo ""
-	    ./bin/gbuild -j ${proc} -m ${mem} --commit prcycoin=${COMMIT} --url prcycoin=${url} ../prcycoin/contrib/gitian-descriptors/gitian-aarch64.yml
-	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-aarch64 --destination ../gitian.sigs/ ../prcycoin/contrib/gitian-descriptors/gitian-aarch64.yml
-	    mv build/out/prcycoin-*.tar.gz build/out/src/prcycoin-*.tar.gz ../prcycoin-binaries/${VERSION}
-	fi
-	popd
+    # Linux
+    if [[ $linux = true ]]
+    then
+        echo ""
+        echo "Compiling ${VERSION} Linux"
+        echo ""
+        ./bin/gbuild -j ${proc} -m ${mem} --commit prcycoin=${COMMIT} --url prcycoin=${url} ../prcycoin/contrib/gitian-descriptors/gitian-linux.yml
+        ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-linux --destination ../gitian.sigs/ ../prcycoin/contrib/gitian-descriptors/gitian-linux.yml
+        mv build/out/prcycoin-*.tar.gz build/out/src/prcycoin-*.tar.gz ../prcycoin-binaries/${VERSION}
+    fi
+    # Windows
+    if [[ $windows = true ]]
+    then
+        echo ""
+        echo "Compiling ${VERSION} Windows"
+        echo ""
+        ./bin/gbuild -j ${proc} -m ${mem} --commit prcycoin=${COMMIT} --url prcycoin=${url} ../prcycoin/contrib/gitian-descriptors/gitian-win.yml
+        ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-win-unsigned --destination ../gitian.sigs/ ../prcycoin/contrib/gitian-descriptors/gitian-win.yml
+        mv build/out/prcycoin-*-win-unsigned.tar.gz inputs/prcycoin-win-unsigned.tar.gz
+        mv build/out/prcycoin-*.zip build/out/prcycoin-*.exe ../prcycoin-binaries/${VERSION}
+    fi
+    # Mac OSX
+    if [[ $osx = true ]]
+    then
+        echo ""
+        echo "Compiling ${VERSION} Mac OSX"
+        echo ""
+        ./bin/gbuild -j ${proc} -m ${mem} --commit prcycoin=${COMMIT} --url prcycoin=${url} ../prcycoin/contrib/gitian-descriptors/gitian-osx.yml
+        ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-osx-unsigned --destination ../gitian.sigs/ ../prcycoin/contrib/gitian-descriptors/gitian-osx.yml
+        mv build/out/prcycoin-*-osx-unsigned.tar.gz inputs/prcycoin-osx-unsigned.tar.gz
+        mv build/out/prcycoin-*.tar.gz build/out/prcycoin-*.dmg ../prcycoin-binaries/${VERSION}
+    fi
+    # AArch64
+    if [[ $aarch64 = true ]]
+    then
+        echo ""
+        echo "Compiling ${VERSION} AArch64"
+        echo ""
+        ./bin/gbuild -j ${proc} -m ${mem} --commit prcycoin=${COMMIT} --url prcycoin=${url} ../prcycoin/contrib/gitian-descriptors/gitian-aarch64.yml
+        ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-aarch64 --destination ../gitian.sigs/ ../prcycoin/contrib/gitian-descriptors/gitian-aarch64.yml
+        mv build/out/prcycoin-*.tar.gz build/out/src/prcycoin-*.tar.gz ../prcycoin-binaries/${VERSION}
+    fi
+    popd
 
-        if [[ $commitFiles = true ]]
-        then
-	    # Commit to gitian.sigs repo
-            echo ""
-            echo "Committing ${VERSION} Unsigned Sigs"
-            echo ""
-            pushd gitian.sigs
-            git add ${VERSION}-linux/${SIGNER}
-            git add ${VERSION}-aarch64/${SIGNER}
-            git add ${VERSION}-win-unsigned/${SIGNER}
-            git add ${VERSION}-osx-unsigned/${SIGNER}
-            git commit -a -m "Add ${VERSION} unsigned sigs for ${SIGNER}"
-            popd
-        fi
+    if [[ $commitFiles = true ]]
+    then
+        # Commit to gitian.sigs repo
+        echo ""
+        echo "Committing ${VERSION} Unsigned Sigs"
+        echo ""
+        pushd gitian.sigs
+        git add ${VERSION}-linux/${SIGNER}
+        git add ${VERSION}-aarch64/${SIGNER}
+        git add ${VERSION}-win-unsigned/${SIGNER}
+        git add ${VERSION}-osx-unsigned/${SIGNER}
+        git commit -a -m "Add ${VERSION} unsigned sigs for ${SIGNER}"
+        popd
+    fi
 fi
 
 # Verify the build
 if [[ $verify = true ]]
 then
-	# Linux
-	pushd ./gitian-builder
-	echo ""
-	echo "Verifying v${VERSION} Linux"
-	echo ""
-	./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-linux ../prcycoin/contrib/gitian-descriptors/gitian-linux.yml
-	# Windows
-	echo ""
-	echo "Verifying v${VERSION} Windows"
-	echo ""
-	./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-win-unsigned ../prcycoin/contrib/gitian-descriptors/gitian-win.yml
-	# Mac OSX
-	echo ""
-	echo "Verifying v${VERSION} Mac OSX"
-	echo ""
-	./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-osx-unsigned ../prcycoin/contrib/gitian-descriptors/gitian-osx.yml
-	# AArch64
-	echo ""
-	echo "Verifying v${VERSION} AArch64"
-	echo ""
-	./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-aarch64 ../prcycoin/contrib/gitian-descriptors/gitian-aarch64.yml
-	# Signed Windows
-	echo ""
-	echo "Verifying v${VERSION} Signed Windows"
-	echo ""
-	./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-osx-signed ../prcycoin/contrib/gitian-descriptors/gitian-osx-signer.yml
-	# Signed Mac OSX
-	echo ""
-	echo "Verifying v${VERSION} Signed Mac OSX"
-	echo ""
-	./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-osx-signed ../prcycoin/contrib/gitian-descriptors/gitian-osx-signer.yml
-	popd
+    # Linux
+    pushd ./gitian-builder
+    echo ""
+    echo "Verifying v${VERSION} Linux"
+    echo ""
+    ./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-linux ../prcycoin/contrib/gitian-descriptors/gitian-linux.yml
+    # Windows
+    echo ""
+    echo "Verifying v${VERSION} Windows"
+    echo ""
+    ./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-win-unsigned ../prcycoin/contrib/gitian-descriptors/gitian-win.yml
+    # Mac OSX
+    echo ""
+    echo "Verifying v${VERSION} Mac OSX"
+    echo ""
+    ./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-osx-unsigned ../prcycoin/contrib/gitian-descriptors/gitian-osx.yml
+    # AArch64
+    echo ""
+    echo "Verifying v${VERSION} AArch64"
+    echo ""
+    ./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-aarch64 ../prcycoin/contrib/gitian-descriptors/gitian-aarch64.yml
+    # Signed Windows
+    echo ""
+    echo "Verifying v${VERSION} Signed Windows"
+    echo ""
+    ./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-osx-signed ../prcycoin/contrib/gitian-descriptors/gitian-osx-signer.yml
+    # Signed Mac OSX
+    echo ""
+    echo "Verifying v${VERSION} Signed Mac OSX"
+    echo ""
+    ./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-osx-signed ../prcycoin/contrib/gitian-descriptors/gitian-osx-signer.yml
+    popd
 fi
 
 # Sign binaries
 if [[ $sign = true ]]
 then
 
-        pushd ./gitian-builder
-	# Sign Windows
-	if [[ $windows = true ]]
-	then
-	    echo ""
-	    echo "Signing ${VERSION} Windows"
-	    echo ""
-	    ./bin/gbuild -i --commit signature=${COMMIT} ../prcycoin/contrib/gitian-descriptors/gitian-win-signer.yml
-	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-win-signed --destination ../gitian.sigs/ ../prcycoin/contrib/gitian-descriptors/gitian-win-signer.yml
-	    mv build/out/prcycoin-*win64-setup.exe ../prcycoin-binaries/${VERSION}
-	    mv build/out/prcycoin-*win32-setup.exe ../prcycoin-binaries/${VERSION}
-	fi
-	# Sign Mac OSX
-	if [[ $osx = true ]]
-	then
-	    echo ""
-	    echo "Signing ${VERSION} Mac OSX"
-	    echo ""
-	    ./bin/gbuild -i --commit signature=${COMMIT} ../prcycoin/contrib/gitian-descriptors/gitian-osx-signer.yml
-	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-osx-signed --destination ../gitian.sigs/ ../prcycoin/contrib/gitian-descriptors/gitian-osx-signer.yml
-	    mv build/out/prcycoin-osx-signed.dmg ../prcycoin-binaries/${VERSION}/prcycoin-${VERSION}-osx.dmg
-	fi
-	popd
+    pushd ./gitian-builder
+    # Sign Windows
+    if [[ $windows = true ]]
+    then
+        echo ""
+        echo "Signing ${VERSION} Windows"
+        echo ""
+        ./bin/gbuild -i --commit signature=${COMMIT} ../prcycoin/contrib/gitian-descriptors/gitian-win-signer.yml
+        ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-win-signed --destination ../gitian.sigs/ ../prcycoin/contrib/gitian-descriptors/gitian-win-signer.yml
+        mv build/out/prcycoin-*win64-setup.exe ../prcycoin-binaries/${VERSION}
+        mv build/out/prcycoin-*win32-setup.exe ../prcycoin-binaries/${VERSION}
+    fi
+    # Sign Mac OSX
+    if [[ $osx = true ]]
+    then
+        echo ""
+        echo "Signing ${VERSION} Mac OSX"
+        echo ""
+        ./bin/gbuild -i --commit signature=${COMMIT} ../prcycoin/contrib/gitian-descriptors/gitian-osx-signer.yml
+        ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-osx-signed --destination ../gitian.sigs/ ../prcycoin/contrib/gitian-descriptors/gitian-osx-signer.yml
+        mv build/out/prcycoin-osx-signed.dmg ../prcycoin-binaries/${VERSION}/prcycoin-${VERSION}-osx.dmg
+    fi
+    popd
 
-        if [[ $commitFiles = true ]]
-        then
-            # Commit Sigs
-            pushd gitian.sigs
-            echo ""
-            echo "Committing ${VERSION} Signed Sigs"
-            echo ""
-            git add ${VERSION}-win-signed/${SIGNER}
-            git add ${VERSION}-osx-signed/${SIGNER}
-            git commit -a -m "Add ${VERSION} signed binary sigs for ${SIGNER}"
-            popd
-        fi
+    if [[ $commitFiles = true ]]
+    then
+        # Commit Sigs
+        pushd gitian.sigs
+        echo ""
+        echo "Committing ${VERSION} Signed Sigs"
+        echo ""
+        git add ${VERSION}-win-signed/${SIGNER}
+        git add ${VERSION}-osx-signed/${SIGNER}
+        git commit -a -m "Add ${VERSION} signed binary sigs for ${SIGNER}"
+        popd
+    fi
 fi
