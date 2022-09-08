cb502b56cd3927b4c3643fc5c21dd578bfc4217f
lyricidal <lyricidal@gmail.com>
2022-09-08 14:35:02 -0700

    Prevent lock spamming when no stakable inputs are present

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3840,6 +3840,7 @@ bool CWallet::CreateCoinStake(
 
     if (listInputs.empty()) {
         LogPrintf("CreateCoinStake(): listInputs empty\n");
+        MilliSleep(50000);
         return false;
     }
 
