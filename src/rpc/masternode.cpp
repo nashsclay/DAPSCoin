81474fb5bda1336d1736da813de243ad1aa3e07f
lyricidal <lyricidal@gmail.com>
2023-01-13 14:00:49 -0500

    [RPC] fix startmasternode "alias/missing" relay

--- src/rpc/masternode.cpp
+++ src/rpc/masternode.cpp
@@ -438,6 +438,8 @@ UniValue startmasternode (const UniValue& params, bool fHelp)
 
             if (result) {
                 successful++;
+                mnodeman.UpdateMasternodeList(mnb);
+                mnb.Relay();
                 statusObj.push_back(Pair("error", ""));
             } else {
                 failed++;
