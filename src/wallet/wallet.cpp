52a31cfcd58839c5e10e73b51742904b1f4fa66a
lyricidal <lyricidal@gmail.com>
2023-01-30 11:06:02 -0500

    [Wallet] Do not select MN Collateral/below Minimum for SelectStakeCoins

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -2291,6 +2291,18 @@ bool CWallet::SelectStakeCoins(std::list<std::unique_ptr<CStakeInput> >& listInp
             if (nAmountSelected + value > nTargetAmount)
                 continue;
 
+            //check that it is above Minimum Stake Amount
+            if (value < Params().MinimumStakeAmount())
+                continue;
+
+            //check that it is not MN Collateral
+            if (value == Params().MNCollateralAmt()) {
+                COutPoint outpoint(out.tx->GetHash(), out.i);
+                if (IsCollateralized(outpoint)) {
+                    continue;
+                }
+            }
+
             int64_t nTxTime = out.tx->GetTxTime();
 
             //check for min age
@@ -2299,10 +2311,6 @@ bool CWallet::SelectStakeCoins(std::list<std::unique_ptr<CStakeInput> >& listInp
 
             //check that it is matured
             if (out.nDepth < (out.tx->IsCoinStake() ? Params().COINBASE_MATURITY() : 10))
-                continue;
-
-            //check that it is above Minimum Stake Amount
-            if (value < Params().MinimumStakeAmount())
                 continue;
 
             //add to our stake set
