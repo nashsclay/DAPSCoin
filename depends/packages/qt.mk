58ffc26aaf6830ad45446af4441fa7962bfc6177
lyricidal <lyricidal@gmail.com>
2023-03-21 18:07:30 -0400

    build: Drop non-existent share/pkgconfig directory

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -235,7 +235,6 @@ endef
 define $(package)_config_cmds
   export PKG_CONFIG_SYSROOT_DIR=/ && \
   export PKG_CONFIG_LIBDIR=$(host_prefix)/lib/pkgconfig && \
-  export PKG_CONFIG_PATH=$(host_prefix)/share/pkgconfig  && \
   ./configure $($(package)_config_opts) && \
   echo "host_build: QT_CONFIG ~= s/system-zlib/zlib" >> mkspecs/qconfig.pri && \
   echo "CONFIG += force_bootstrap" >> mkspecs/qconfig.pri && \
