6bdbf124a6b03cc4946e1b7dec2fdaea260ce73a
lyricidal <lyricidal@gmail.com>
2023-06-26 15:23:52 -0400

    Use std::make_unique

--- src/util.h
+++ src/util.h
@@ -190,10 +190,11 @@ fs::path AbsPathForConfigVal(const boost::filesystem::path& path, bool net_speci
 bool PointHashingSuccessively(const CPubKey& pk, const unsigned char* tweak, unsigned char* out);
 
 //! Substitute for C++14 std::make_unique.
+//! DEPRECATED use std::make_unique in new code.
 template <typename T, typename... Args>
 std::unique_ptr<T> MakeUnique(Args&&... args)
 {
-    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+    return std::make_unique<T>(std::forward<Args>(args)...);
 }
 
 #endif // BITCOIN_UTIL_H
