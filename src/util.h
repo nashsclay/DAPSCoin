8116b5e27e8319307b238c4a0a435de38cb9090b
lyricidal <lyricidal@gmail.com>
2023-06-26 12:51:27 -0400

    Add MakeUnique (substitute for C++14 std::make_unique)

--- src/util.h
+++ src/util.h
@@ -189,4 +189,11 @@ void TraceThread(const char* name, Callable func)
 fs::path AbsPathForConfigVal(const boost::filesystem::path& path, bool net_specific = true);
 bool PointHashingSuccessively(const CPubKey& pk, const unsigned char* tweak, unsigned char* out);
 
+//! Substitute for C++14 std::make_unique.
+template <typename T, typename... Args>
+std::unique_ptr<T> MakeUnique(Args&&... args)
+{
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
 #endif // BITCOIN_UTIL_H
