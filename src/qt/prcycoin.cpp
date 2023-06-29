eddc4304707270aa132090d20cb425abe53428e5
lyricidal <lyricidal@gmail.com>
2023-06-29 16:39:21 -0400

    Set AA_EnableHighDpiScaling attribute early
    Qt docs: This attribute must be set before QGuiApplication is
    constructed.

--- src/qt/prcycoin.cpp
+++ src/qt/prcycoin.cpp
@@ -585,15 +585,16 @@ int main(int argc, char* argv[])
     Q_INIT_RESOURCE(prcycoin_locale);
     Q_INIT_RESOURCE(prcycoin);
 
-    BitcoinApplication app(argc, argv);
     // Generate high-dpi pixmaps
     QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 #if QT_VERSION >= 0x050600
-    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
 #ifdef Q_OS_MAC
     QApplication::setAttribute(Qt::AA_DontShowIconsInMenus);
 #endif
+
+    BitcoinApplication app(argc, argv);
 
     // Register meta types used for QMetaObject::invokeMethod
     qRegisterMetaType<bool*>();
