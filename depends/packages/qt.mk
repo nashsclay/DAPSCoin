e5a9fd05e59c2260868a978a3e02acf6323e6999
lyricidal <lyricidal@gmail.com>
2023-06-30 18:11:12 -0400

    build: Add ability to build qt in depends with -stdlib=libc++

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -127,7 +127,11 @@ $(package)_config_opts_linux += -ltcg
 endif
 $(package)_config_opts_arm_linux += -platform linux-g++ -xplatform bitcoin-linux-g++
 $(package)_config_opts_i686_linux  = -xplatform linux-g++-32
+ifneq (,$(findstring -stdlib=libc++,$($(1)_cxx)))
+$(package)_config_opts_x86_64_linux = -xplatform linux-clang-libc++
+else
 $(package)_config_opts_x86_64_linux = -xplatform linux-g++-64
+endif
 $(package)_config_opts_aarch64_linux = -xplatform linux-aarch64-gnu-g++
 $(package)_config_opts_riscv64_linux = -platform linux-g++ -xplatform bitcoin-linux-g++
 $(package)_config_opts_s390x_linux += -platform linux-g++ -xplatform linux-g++-64
